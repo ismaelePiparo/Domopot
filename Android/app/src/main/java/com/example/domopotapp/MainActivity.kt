fe1d625e49524f73e80d1541a8bd90d5cfc604ab
ismaelePiparo <73647316+ismaelePiparo@users.noreply.github.com>
2022-03-07 18:24:49 +0100

    Aggiunta scelta reti wifi
    Aggiunti tutti i permessi nel manifest file e primo abbozzo del primo step di configurazione del esp8266

--- Android/app/src/main/java/com/example/domopotapp/MainActivity.kt
+++ Android/app/src/main/java/com/example/domopotapp/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.domopotapp
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +10,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Verifica la versione di Android perchè dalla versione 10 servono dei permessi per usare WiFiManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_WIFI_STATE,
+                Manifest.permission.ACCESS_NETWORK_STATE), 0)
+            //Permessi necessari da Android 10 in poi
+
+        } else {
+            //per le versioni precedenti (9, 8...) non serve nessun permesso particolare
+        }
     }
 }
--- Android/app/src/main/java/com/example/domopotapp/ui/main/ConfigStep1.kt
+++ Android/app/src/main/java/com/example/domopotapp/ui/main/ConfigStep1.kt
@@ -1,9 +1,17 @@
 package com.example.domopotapp.ui.main
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.wifi.WifiManager
 import android.os.Bundle
+import android.provider.Settings
 import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 
 import com.example.domopotapp.R
@@ -18,19 +26,78 @@ class ConfigStep1 : Fragment(R.layout.fragment_config_setp_1) {
         }
     }
 
+    private val viewModel by activityViewModels<MainViewModel>()
+
+    private val TAG = ConfigStep1::class.java.name
+
+    private var wifiManager: WifiManager? = null
+    private lateinit var scanBtn: Button
+    private lateinit var nextBtn: Button
+    private lateinit var wifiBtn: Button
+    private lateinit var infoWifi: TextView
+    private var currentWifi: String =""
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val next=view.findViewById<Button>(R.id.add)
-        val back=view.findViewById<Button>(R.id.back)
 
-        next.setOnClickListener{
+        nextBtn=view.findViewById<Button>(R.id.next)
+        infoWifi=view.findViewById<TextView>(R.id.current_wifi)
+        scanBtn=view.findViewById<Button>(R.id.scanBtn)
+        wifiBtn=view.findViewById<Button>(R.id.wifiMenu)
+        wifiManager = requireActivity().getApplicationContext().getSystemService(Context.WIFI_SERVICE) as WifiManager
+
+        wifiBtn.setOnClickListener {
+            val intent = Intent(Settings.ACTION_WIFI_SETTINGS)
+            startActivity(intent)
+        }
+
+        nextBtn.isEnabled=false
+        nextBtn.setOnClickListener{
             findNavController().navigate(R.id.action_configStep_1_to_configStep_2)
         }
-
-        back.setOnClickListener{
-            findNavController().navigate(R.id.action_configStep_1_to_home)
+    }
+    var wifiStatus: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            val wifiStateExtra = intent.getIntExtra(
+                WifiManager.EXTRA_WIFI_STATE,
+                WifiManager.WIFI_STATE_UNKNOWN
+            )
+            when (wifiStateExtra) {
+                WifiManager.WIFI_STATE_ENABLED -> {
+                    infoWifi.text="connessione in corso..."
+                }
+                WifiManager.WIFI_STATE_DISABLED -> {
+                    infoWifi.text="NO WIFI"
+                }
+            }
         }
+    }
 
+    var networkStatus: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            if(wifiManager!!.connectionInfo.ssid.equals("<unknown ssid>") && wifiManager!!.isWifiEnabled){
+                infoWifi.text = "connessione in corso..."
+            }else if (!wifiManager!!.isWifiEnabled){
+                infoWifi.text="NO WIFI"
+            }else{
+                infoWifi.text=wifiManager!!.connectionInfo.ssid
+                if(wifiManager!!.connectionInfo.ssid.equals("\"" + "AndroidWifi" + "\"")){
+                    nextBtn.isEnabled=true
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        requireActivity().registerReceiver(wifiStatus, IntentFilter(WifiManager.WIFI_STATE_CHANGED_ACTION))
+        requireActivity().registerReceiver(networkStatus, IntentFilter(WifiManager.NETWORK_STATE_CHANGED_ACTION))
+    }
+
+    override fun onPause(){
+        super.onPause()
+        requireActivity().unregisterReceiver(wifiStatus)
+        requireActivity().unregisterReceiver(networkStatus)
     }
 }
