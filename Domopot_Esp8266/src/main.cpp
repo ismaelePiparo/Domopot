daebbdf4bbff77c6d4530bc7884ae680a8d56a05
abenfante <abenfante97@gmail.com>
2022-03-15 00:21:26 +0100

    Inizio integrazione codice di Isma

--- Domopot_Esp8266/src/main.cpp
+++ Domopot_Esp8266/src/main.cpp
@@ -1,7 +1,26 @@
-#include <Arduino.h>
+/*#include <Arduino.h>
 #include <Wire.h>
+#include <ESP8266Firebase.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
 
 #define bytes 6
+#define PROJECT_ID "domopotdb-default-rtdb"  
+
+const char* ap_ssid = "DomoPot_WiFi"; //Access Point SSID
+const char* ap_password= ""; //Access Point Password
+uint8_t max_connections=8;//Maximum Connection Limit for AP
+int current_stations=0, new_stations=0;
+
+ESP8266WebServer server(80);
+Firebase firebase(PROJECT_ID);
+
+//Inizializzazione delle variabili. ssid e passwordo sono quelli che verranno passati via app
+bool onLine = false;
+String Pot_ID = "DomoPot_01";
+String ssid = "UNKNOWN";
+String pass = "UNKNOWN";
 
 float humidity = 0;
 int waterLvl = 0;
@@ -18,6 +37,18 @@ int requestData(void);
 void setup() {
   Serial.begin(115200);
   Wire.begin();
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN,HIGH);
+
+  for (uint8_t t = 4; t > 0; t--) {
+    Serial.printf("[SETUP] WAIT %d...\n", t);
+    Serial.flush();
+    delay(1000);
+  }
+  
+  
+
 }
 
 void loop() {
@@ -50,4 +81,4 @@ int requestData(){
     Serial.println("No correct answer from arduino :(");
     return 1;
   }
-}
+}*/
