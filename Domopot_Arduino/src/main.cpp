672f38863aacfcd4577901d3779adfac5696a0a3
abenfante <abenfante97@gmail.com>
2022-03-17 14:15:10 +0100

    Programmate luci AP

--- Domopot_Arduino/src/main.cpp
+++ Domopot_Arduino/src/main.cpp
@@ -2,6 +2,12 @@
 #include <Wire.h>
 #include "FastLED.h"
 
+/*** APPUNTI
+ * Nota bene: tutte le funzioni di animazione dei led sono pensate per non
+ * essere bloccanti e per essere chiamate ripetutamente nel loop principale.
+***/
+
+
 #pragma region Macros
 //pins
 #define echoPin 3 // attach pin D2 Arduino to pin Echo of HC-SR04
@@ -10,14 +16,13 @@
 #define humPin A0 // pin sensore umidità
 
 //Misura umidità
-#define humSamples 5 //numero di campioni su cui fare la media dell'umidità
+#define humSamples 1 //numero di campioni su cui fare la media dell'umidità
 #define humTime 100 //tempo tra una misura dell'umidità e la successiva in ms
 
 //Parametri LEDs
 #define NUM_LEDS 5 //numero di led
 #define depth 30  //profondità massima da misurare
-#define tono 180 //tono del colore dell'indicatore (hue in HSV)
-#define alpha 0.5 //sensibilità del sensore (veloce -> jitter, lento -> poco responsivo)
+#define alpha 0.3 //sensibilità del sensore (veloce -> jitter, lento -> poco responsivo)
 #pragma endregion
 
 // Variabili
@@ -43,6 +48,7 @@ void ShowDistanceAnimation();
 void LedsOff();
 float MeasureHumidity();
 void ReceiveState(int n);
+void AccessPointAnimation();
 #pragma endregion
 
 void setup() {
@@ -57,6 +63,13 @@ void setup() {
 }
 
 void loop() {
+  
+  //Loop tra stati per testare i led (codice temporaneo)
+  if((millis() % 6000) < 3000){
+    ledState = accessPoint;
+  }else{
+    ledState = accessPoint;
+  }
 
   // Calcola la distanza e mostrala sui led o mostra un errore
   switch(ledState){
@@ -64,7 +77,7 @@ void loop() {
         ShowDistance();
         break;
     case accessPoint:
-        Serial.println("Access point behaviour to implement");
+        AccessPointAnimation();
         break;
     case connected:
         Serial.println("Connection behaviour not implemented yet");
@@ -73,13 +86,14 @@ void loop() {
         LedsOff();
         break;
   }
-
+  
   Serial.print("Distance: ");
   Serial.print(distance);
   Serial.println(" cm");
   Serial.print("Humidity: ");
   Serial.print(MeasureHumidity());
   Serial.println(" V");
+  
 }
 
 void sendData() {
@@ -105,19 +119,20 @@ int MeasureDistance() {
 
 void OutOfRangeAnimation(){
   count++;  //avanza animazione di out-of-range
-    int phase = count % (NUM_LEDS * 2 - 2);
-    for (int i = 0; i < NUM_LEDS; i++) {
-      if (phase >= NUM_LEDS) {
-        phase = (NUM_LEDS * 2 - 2) - phase ;
-      }
-      if (i == phase) {
-        leds[i] = CRGB::Red;
-      } else {
-        leds[i] = CRGB::Black;
-      }
+  count = millis() / 300;
+  
+  int phase = count % (NUM_LEDS * 2 - 2);
+  for (int i = 0; i < NUM_LEDS; i++) {
+    if (phase >= NUM_LEDS) {
+      phase = (NUM_LEDS * 2 - 2) - phase ;
     }
-    FastLED.show();
-    delay(300);
+    if (i == phase) {
+     leds[i] = CRGB::Red;
+    } else {
+      leds[i] = CRGB::Black;
+    }
+  }
+  FastLED.show();
 }
 
 void ShowDistance(){
@@ -133,16 +148,17 @@ void ShowDistance(){
 void ShowDistanceAnimation(){
     int interval = distance / NUM_LEDS;
     int subInterval = distance % NUM_LEDS;
+    int waterHue = 130;
 
     for (int i = 0; i < NUM_LEDS; i++) {
       if (i > interval) {   // led acceso
-        leds[i] = CHSV(tono, 255, 255);
+        leds[i] = CHSV(waterHue, 255, 255);
       }
       else if (i < interval) {    //led spento
         leds[i] = CRGB::Black;
       }
       else {  //led dimmato
-        leds[i] = CHSV(tono, 255, ((255 - subInterval) * 255) / NUM_LEDS);
+        leds[i] = CHSV(waterHue, 255, ((255 - subInterval) * 255) / NUM_LEDS);
       }
     }
     FastLED.show();
@@ -151,6 +167,15 @@ void ShowDistanceAnimation(){
 
 void LedsOff(){
   FastLED.clear(true);
+}
+
+void AccessPointAnimation(){
+  int orangeHue = 32;
+  int value = (1 + sin(0.3*float(millis())/1024*6.28)) * 90 + 70;
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CHSV(orangeHue, 255, value);
+  }
+  FastLED.show();
 }
 
 float MeasureHumidity(){ //per ora ritorna la tensione misurata dal sensore
