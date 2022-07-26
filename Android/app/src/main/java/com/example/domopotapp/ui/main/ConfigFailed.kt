b57991dc410e571cbf6a97035128861e587ccadc
ismaelePiparo <73647316+ismaelePiparo@users.noreply.github.com>
2022-07-26 16:51:38 +0200

    bloccato back in configurazione

--- Android/app/src/main/java/com/example/domopotapp/ui/main/ConfigFailed.kt
+++ Android/app/src/main/java/com/example/domopotapp/ui/main/ConfigFailed.kt
@@ -1,8 +1,10 @@
 package com.example.domopotapp.ui.main
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.domopotapp.R
@@ -20,6 +22,13 @@ class ConfigFailed : Fragment(R.layout.config_failed_fragment) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        //disabilita il tasto back
+        val callback = requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+            Log.w("BACK PRESSED","Attendere il completamento dell'operazione...")
+            //Toast.makeText(activity, "Attendere il completamento dell'operazione...", Toast.LENGTH_LONG).show()
+
+        }
 
         val backToHomeButton: Button = view.findViewById(R.id.configFailedBackToHomeBtn)
         val retryButton: Button = view.findViewById(R.id.configFailedRetryBtn)
